6ccde72c4031787f60ee2cf570e034aae2bb0bf6
raphaelhild <raphael.daniel.hild@hs-furtwangen.de>
2022-05-16 12:52:25 +0200

    Increase and decrease Circle Radius, Set new Locations correctly, Preparation for marker-updating (distance to circle-center), Simple Layout-Adjustments

--- app/src/main/java/com/example/scapp_scouting/MapsActivity.kt
+++ app/src/main/java/com/example/scapp_scouting/MapsActivity.kt
@@ -2,7 +2,7 @@ package com.example.scapp_scouting
 
 import android.graphics.Color
 import android.location.Geocoder
-import android.location.LocationManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -23,9 +23,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
     private lateinit var searchView: SearchView
 
+    private lateinit var currentMapLocation: LatLng
+    private var currentCircleRadius: Double = 1000.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -45,17 +47,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 return false
             }
         })
-    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        //TODO: currentMapLocation mit aktuellem Standort definieren?
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -65,50 +59,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isMapToolbarEnabled = false // Toolbar ("Open Maps and Route to - Symbols")
         mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE // Map-Style
 
-
         // Geocoding and Marker
         val coordinates = geocoder.getFromLocationName("Furtwangen,I-Bau", 1)  // add these two lines
-        val location = LatLng(coordinates[0].latitude, coordinates[0].longitude)
-        mMap.addCircle(
+        currentMapLocation = LatLng(coordinates[0].latitude, coordinates[0].longitude)
+        mMap.addCircle(             // Generierung des ersten Kreises, bzw. Radius für die Anzeige von Markern
             CircleOptions()
-                .center(location)
+                .center(currentMapLocation)
                 .radius(1000.0)
                 .strokeColor(Color.RED)
                 .fillColor(0x22ff0000)
         )
-        mMap.addMarker(
+        mMap.addMarker(             // Test zum Setzen eines Markers
             MarkerOptions()
-                .position(location)
+                .position(currentMapLocation)
                 .title(coordinates[0].postalCode.toString() + " " + coordinates[0].featureName.toString())
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_icon_bounded)) // alternatively: marker_icon
                 .snippet(coordinates[0].countryName.toString())
         )
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentMapLocation, 14.0f))
     }
-
-
 
     fun setNewLocation(newLocation: String){
        val geocoder = Geocoder(this)
        try {
            val coordinates = geocoder.getFromLocationName(newLocation, 1)  // add these two lines
-           val location = LatLng(coordinates[0].latitude, coordinates[0].longitude)
-           mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f))
-           mMap.clear()                             //Bestehende Kreise und Marker entfernen
-           mMap.addCircle(                          //Kreis im Standard-Radius einfügen
+           currentMapLocation = LatLng(coordinates[0].latitude, coordinates[0].longitude)
+           currentCircleRadius = 1000.0             // Zurücksetzen des Circle-Radius
+           mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentMapLocation, 14.0f))
+           mMap.clear()                             // Bestehende Kreise und Marker entfernen
+           mMap.addCircle(                          // Kreis im Standard-Radius einfügen
                CircleOptions()
-                   .center(location)
-                   .radius(1000.0)
+                   .center(currentMapLocation)
+                   .radius(currentCircleRadius)
                    .strokeColor(Color.RED)
                    .fillColor(0x22ff0000)
            )
-
-           //TODO: Alle möglichen Marker im Umkreis aus der Datenbank laden
+           updateMarkerInRadius()                   // Marker im Radius neu setzen
 
        } catch (e: Exception) {
            Log.e("MapErrors", "Keine Koordinaten zur Zieleingabe gefunden. Fehlermeldung: " + e);
        }
 
+    }
+
+    fun increaseCircleRadius(view: View){
+        currentCircleRadius += 200               // Radius vergrößern
+        updateCircleRadius(currentCircleRadius)
+    }
+
+    fun decreaseCircleRadius(view: View){
+        currentCircleRadius -= 200               // Radius verkleinern
+        updateCircleRadius(currentCircleRadius)
+    }
+
+    fun updateCircleRadius(radius: Double){
+        mMap.clear()                             // Bestehende Kreise und Marker entfernen
+        mMap.addCircle(                          // Kreis mit vorgegebenem Radius einfügen
+            CircleOptions()
+                .center(currentMapLocation)
+                .radius(radius)
+                .strokeColor(Color.RED)
+                .fillColor(0x22ff0000)
+        )
+        updateMarkerInRadius()                   // Marker im Radius neu setzen
+    }
+
+    fun updateMarkerInRadius(){
+        // TODO: Hier dann jedes mal alle Marker im Radius laden
+        // Überprüfung der Marker innerhalb des Radius mit getDistanceBetweenTwoPoints()
     }
 
     fun changeMapType(view: View){
@@ -122,5 +140,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         else{
             mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
         }
+    }
+
+    fun getDistanceBetweenTwoPoints(start: LatLng, end: LatLng) : Float {
+        val startPoint = Location("locationA")
+        startPoint.setLatitude(start.latitude)
+        startPoint.setLongitude(start.longitude)
+
+        val endPoint = Location("locationA")
+        endPoint.setLatitude(end.latitude)
+        endPoint.setLongitude(end.longitude)
+
+        return startPoint.distanceTo(endPoint)
     }
 }
